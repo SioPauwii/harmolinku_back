d2facd61950d6065376dc05808553521bef2513b
GCCoEdAdmin <gccoed@gmail.com>
2025-06-06 01:53:03 +0000

    Change db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const db = mysql.createPool({
-  host: "localhost",
+  host: "mysql-harmonilink.alwaysdata.net",
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
